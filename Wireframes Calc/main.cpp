4ed32a5bd94736a9c9f771e01da87689a0dd2b09
Aaron Pfitzenmaier <aaron@pfitzpr.com>
2016-06-29 21:16:01 -0500

    Initial Commit

---
+++ Wireframes Calc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Wireframes Calc
+//
+//  Created by Aaron Pfitzenmaier on 6/29/16.
+//  Copyright (c) 2016 Aaron Pfitzenmaier. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
